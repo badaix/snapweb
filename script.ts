9ef5c5a5b7537652fcc207399550afa16e5ea3c4
badaix <johannes.pohl@badaix.de>
2020-05-20 13:54:06 +0200

    Add SnapControl class

--- script.ts
+++ script.ts
@@ -113,8 +113,9 @@ class Stream {
 }
 
 class Server {
-    constructor(json: any) {
-        this.fromJson(json);
+    constructor(json?: any) {
+        if (json)
+            this.fromJson(json);
     }
 
     fromJson(json: any) {
@@ -169,84 +170,158 @@ class Server {
 
 
 
-//let connection = new WebSocket('ws://' + window.location.hostname + ':1780/jsonrpc');
-let connection = new WebSocket('ws://127.0.0.1:1780/jsonrpc');
-let server: Server;
-let msg_id: number = 23;
-let status_req_id: number = -1;
+class SnapControl {
+    constructor(host: string, port: number) {
+        this.server = new Server();
+        this.connection = new WebSocket('ws://' + host + ':' + port + '/jsonrpc');
+        this.msg_id = 0;
+        this.status_req_id = -1;
 
-connection.onmessage = function (msg: MessageEvent) {
-    let recv = msg.data
-    // console.log(recv); 
-    let answer = JSON.parse(recv);
-    console.log(answer)
-    if (answer.id == status_req_id) {
-        server = new Server(answer.result.server);
-    } else if (Array.isArray(answer)) {
-        for (let a of answer) {
-            action(a);
+        this.connection.onmessage = (msg: MessageEvent) => this.onMessage(msg.data);
+        this.connection.onopen = (ev: Event) => { this.status_req_id = this.sendRequest('Server.GetStatus'); }
+        this.connection.onerror = (ev: Event) => { alert("error: " + ev.type); }//this.onError(ev);
+    }
+
+    private action(answer: any) {
+        switch (answer.method) {
+            case 'Client.OnVolumeChanged':
+                (this.getClient(answer.params.id) as Client).config.volume = answer.params.volume;
+                break;
+            case 'Client.OnLatencyChanged':
+                (this.getClient(answer.params.id) as Client).config.latency = answer.params.latency;
+                break;
+            case 'Client.OnNameChanged':
+                (this.getClient(answer.params.id) as Client).config.name = answer.params.name;
+                break;
+            case 'Client.OnConnect':
+            case 'Client.OnDisconnect':
+                (this.getClient(answer.params.client.id) as Client).fromJson(answer.params.client);
+                break;
+            case 'Group.OnMute':
+                (this.getGroup(answer.params.id) as Group).muted = Boolean(answer.params.mute);
+                break;
+            case 'Group.OnStreamChanged':
+                (this.getGroup(answer.params.id) as Group).stream_id = answer.params.stream_id;
+                break;
+            case 'Stream.OnUpdate':
+                (this.getStream(answer.params.id) as Stream).fromJson(answer.params.stream);
+                break;
+            case 'Server.OnUpdate':
+                this.server.fromJson(answer.params.server);
+                break;
+            default:
+                break;
         }
-    } else {
-        action(answer);
     }
-    show()
-}
 
-connection.onopen = function () {
-    status_req_id = send('Server.GetStatus');
-}
-
-connection.onerror = function () {
-    alert("error");
-}
-
-function action(answer: any) {
-    switch (answer.method) {
-        case 'Client.OnVolumeChanged':
-            (server.getClient(answer.params.id) as Client).config.volume = answer.params.volume;
-            break;
-        case 'Client.OnLatencyChanged':
-            (server.getClient(answer.params.id) as Client).config.latency = answer.params.latency;
-            break;
-        case 'Client.OnNameChanged':
-            (server.getClient(answer.params.id) as Client).config.name = answer.params.name;
-            break;
-        case 'Client.OnConnect':
-        case 'Client.OnDisconnect':
-            (server.getClient(answer.params.client.id) as Client).fromJson(answer.params.client);
-            break;
-        case 'Group.OnMute':
-            (server.getGroup(answer.params.id) as Group).muted = Boolean(answer.params.mute);
-            break;
-        case 'Group.OnStreamChanged':
-            (server.getGroup(answer.params.id) as Group).stream_id = answer.params.stream_id;
-            break;
-        case 'Stream.OnUpdate':
-            (server.getStream(answer.params.id) as Stream).fromJson(answer.params.stream);
-            break;
-        case 'Server.OnUpdate':
-            server.fromJson(answer.params.server);
-            break;
-        default:
-            break;
+    public getClient(client_id: string): Client | null {
+        return this.server.getClient(client_id);
     }
+
+    public getGroup(group_id: string): Group | null {
+        return this.server.getGroup(group_id);
+    }
+
+    public getGroupFromClient(client_id: string): Group | null {
+        for (let group of this.server.groups)
+            for (let client of group.clients)
+                if (client.id == client_id)
+                    return group;
+        return null;
+    }
+
+    public getStream(stream_id: string): Stream | null {
+        return this.server.getStream(stream_id);
+    }
+
+    public setVolume(client_id: string, percent: number, mute: boolean) {
+        percent = Math.max(0, Math.min(100, percent));
+        (this.getClient(client_id) as Client).config.volume = { muted: mute, percent: percent };
+        this.sendRequest('Client.SetVolume', '{"id":"' + client_id + '","volume":{"muted":' + (mute ? "true" : "false") + ',"percent":' + percent + '}}');
+    }
+
+    public setClientName(client_id: string, name: string) {
+        let client = this.getClient(client_id) as Client;
+        let current_name: string = (client.config.name != "") ? client.config.name : client.host.name;
+        if (name != current_name) {
+            this.sendRequest('Client.SetName', '{"id":"' + client_id + '","name":"' + name + '"}');
+            client.config.name = name;
+        }
+    }
+
+    public setClientLatency(client_id: string, latency: number) {
+        let client = this.getClient(client_id) as Client;
+        let current_latency: number = client.config.latency;
+        if (latency != current_latency) {
+            this.sendRequest('Client.SetLatency', '{"id":"' + client_id + '","latency":"' + latency + '"}');
+            client.config.latency = latency;
+        }
+    }
+
+    public setStream(group_id: string, stream_id: string) {
+        (this.getGroup(group_id) as Group).stream_id = stream_id;
+        this.sendRequest('Group.SetStream', '{"id":"' + group_id + '","stream_id":"' + stream_id + '"}');
+    }
+
+    public setClients(group_id: string, clients: string[]) {
+        this.status_req_id = this.sendRequest('Group.SetClients', '{"clients":' + JSON.stringify(clients) + ',"id":"' + group_id + '"}');
+    }
+
+    public muteGroup(group_id: string, mute: boolean) {
+        (this.getGroup(group_id) as Group).muted = mute;
+        this.sendRequest('Group.SetMute', '{"id":"' + group_id + '","mute":' + (mute ? "true" : "false") + '}');
+    }
+
+    public sendRequest(method: string, params?: string): number {
+        let msg = '{"id": ' + (++this.msg_id) + ',"jsonrpc":"2.0","method":"' + method + '"';
+        if (params)
+            msg += ',"params": ' + params;
+        msg += '}';
+        console.log("Sending: " + msg);
+        this.connection.send(msg);
+        return this.msg_id;
+    }
+
+    private onMessage(msg: string) {
+        let answer = JSON.parse(msg);
+        let is_response: boolean = (answer.id != undefined);
+        console.log("Received " + (is_response ? "response" : "notification") + ", json: " + JSON.stringify(answer))
+        if (is_response) {
+            if (answer.id == this.status_req_id) {
+                this.server = new Server(answer.result.server);
+                show();
+            }
+        }
+        else {
+            if (Array.isArray(answer)) {
+                for (let a of answer) {
+                    this.action(a);
+                }
+            } else {
+                this.action(answer);
+            }
+        }
+    }
+
+    connection: WebSocket;
+    server: Server;
+    msg_id: number;
+    status_req_id: number;
 }
 
-function send(method: string, params?: string): number {
-    let msg = '{"id": ' + (++msg_id) + ',"jsonrpc":"2.0","method":"' + method + '"';
-    if (params)
-        msg += ',"params": ' + params;
-    msg += '}';
-    console.log("Sending: " + msg);
-    connection.send(msg);
-    return msg_id;
-}
 
+//let connection = new WebSocket('ws://' + window.location.hostname + ':1780/jsonrpc');
+// let connection = new WebSocket('ws://127.0.0.1:1780/jsonrpc');
+// let server: Server;
+// let msg_id: number = 23;
+// let status_req_id: number = -1;
+let snapcontrol = new SnapControl("127.0.0.1", 1780);
 
 function show() {
     // Render the page
     let content = "";
 
+    let server = snapcontrol.server;
     for (let group of server.groups) {
         // Set mute variables
         let classgroup;
@@ -323,7 +398,7 @@ function show() {
 
             // Populate client div
             content = content + " <a href=\"javascript:setVolume('" + client.id + "'," + !muted + ");\" class='mutebutton'>" + mutetext + "</a>";
-            content = content + "<div class='sliders'><div class='sliderdiv'><input type='range' min=0 max=100 step=1 id='vol_" + client.id + "' onchange='javascript:setVolume(\"" + client.id + "\"," + client.config.volume.muted + ")' value=" + client.config.volume.percent + " class='" + sliderclass + "' orient='vertical'></div>";
+            content = content + "<div class='sliders'><div class='sliderdiv'><input type='range' min=0 max=100 step=1 id='vol_" + client.id + "' oninput='javascript:setVolume(\"" + client.id + "\"," + client.config.volume.muted + ")' value=" + client.config.volume.percent + " class='" + sliderclass + "' orient='vertical'></div>";
             content = content + "<div class='finebg'>++<br>+<br>0<br>-<br>--</div><div class='sliderdiv_fine'><input type='range' min=0 max=10 step=1 id='vol_fine_" + client.id + "' onchange='javascript:setVolume(\"" + client.id + "\"," + client.config.volume.muted + ")' value=5 class='" + sliderclass + "_fine' orient='vertical'></div></div>";
             content = content + " <a href=\"javascript:setName('" + client.id + "');\" class='edit'>&#9998</a>";
             content = content + name;
@@ -340,29 +415,22 @@ function show() {
 }
 
 function setVolume(id: string, mute: boolean) {
+    console.log("setVolume id: " + id + ", mute: " + mute);
     let percent = Number((document.getElementById('vol_' + id) as HTMLInputElement).value);
     let percent_fine = Number((document.getElementById('vol_fine_' + id) as HTMLInputElement).value);
 
-    // Take away 5 as it's the default of the fine slider. Only relevant if it
-    // has changed
-    percent = Number(percent) + Number(percent_fine) - 5;
-    percent = Math.max(0, Math.min(100, percent));
-
-    // Request changes
-    send('Client.SetVolume', '{"id":"' + id + '","volume":{"muted":' + (mute ? "true" : "false") + ',"percent":' + percent + '}}');
-    (server.getClient(id) as Client).config.volume = { muted: mute, percent: percent };
-    show()
+    // Take away 5 as it's the default of the fine slider. Only relevant if it has changed
+    snapcontrol.setVolume(id, Number(percent) + Number(percent_fine) - 5, mute);
+    //show()
 }
 
 function setMuteGroup(id: string, mute: boolean) {
-    send('Group.SetMute', '{"id":"' + id + '","mute":' + (mute ? "true" : "false") + '}');
-    (server.getGroup(id) as Group).muted = mute;
+    snapcontrol.muteGroup(id, mute);
     show()
 }
 
 function setStream(id: string) {
-    send('Group.SetStream', '{"id":"' + id + '","stream_id":"' + (document.getElementById('stream_' + id) as HTMLInputElement).value + '"}');
-    (server.getGroup(id) as Group).stream_id = (document.getElementById('stream_' + id) as HTMLInputElement).value;
+    snapcontrol.setStream(id, (document.getElementById('stream_' + id) as HTMLInputElement).value);
     show()
 }
 
@@ -370,17 +438,9 @@ function setGroup(id: string) {
     let group = (document.getElementById('group_' + id) as HTMLInputElement).value;
     console.log("setGroup id: " + id + ", group: " + group);
 
+    let server = snapcontrol.server;
     // Get client group id
-    let current_group: string = "";
-    groups:
-    for (let i_group = 0; i_group < server.groups.length; i_group++) {
-        for (let i_client = 0; i_client < server.groups[i_group].clients.length; i_client++) {
-            if (id == server.groups[i_group].clients[i_client].id) {
-                current_group = server.groups[i_group].id;
-                break groups;
-            }
-        }
-    }
+    let current_group = snapcontrol.getGroupFromClient(id) as Group;
 
     // Get
     //   List of target group's clients
@@ -388,7 +448,7 @@ function setGroup(id: string) {
     //   List of current group's other clients
     let send_clients = [];
     for (let i_group = 0; i_group < server.groups.length; i_group++) {
-        if (server.groups[i_group].id == group || (group == "new" && server.groups[i_group].id == current_group)) {
+        if (server.groups[i_group].id == group || (group == "new" && server.groups[i_group].id == current_group.id)) {
             for (let i_client = 0; i_client < server.groups[i_group].clients.length; i_client++) {
                 if (group == "new" && server.groups[i_group].clients[i_client].id == id) { }
                 else {
@@ -398,34 +458,26 @@ function setGroup(id: string) {
         }
     }
 
-    if (group == "new") {
-        group = current_group
-    }
-    else {
+    if (group == "new")
+        group = current_group.id;
+    else
         send_clients[send_clients.length] = id;
-    }
 
-    let send_clients_string = JSON.stringify(send_clients);
-    status_req_id = send('Group.SetClients', '{"clients":' + send_clients_string + ',"id":"' + group + '"}');
+    snapcontrol.setClients(group, send_clients);
 }
 
 function setName(id: string) {
     // Get current name and lacency
-    let client = server.getClient(id) as Client;
+    let client = snapcontrol.getClient(id) as Client;
     let current_name: string = (client.config.name != "") ? client.config.name : client.host.name;
     let current_latency: number = client.config.latency;
 
     let new_name = window.prompt("New Name", current_name);
     let new_latency = Number(window.prompt("New Latency", String(current_latency)));
 
-    // Don't change anything if user cancel's
-    if (new_name != null && new_name != current_name) {
-        send('Client.SetName', '{"id":"' + id + '","name":"' + new_name + '"}');
-        client.config.name = new_name;
-    }
-    if (new_latency != null && new_latency != current_latency) {
-        send('Client.SetLatency', '{"id":"' + id + '","latency":' + new_latency + '}');
-        client.config.latency = new_latency;
-    }
+    if (new_name != null)
+        snapcontrol.setClientName(id, new_name);
+    if (new_latency != null)
+        snapcontrol.setClientLatency(id, new_latency);
     show()
 }
