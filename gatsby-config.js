d101d416280f6f41ad403c39f3d0c8f731246c7a
Daniel Evans <me@daredoes.work>
2022-03-11 20:01:09 -0800

    remove unused images

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,15 +28,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `snapweb`,
+        short_name: `Snapweb`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#607d8b`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#607d8b`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
