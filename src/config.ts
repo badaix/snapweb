60946acdc1ff63d1067064073841464f602d9968
Winston Hoy <winston@curiousercreative.com>
2023-10-04 16:16:05 -0400

    check env var for snapserver host and and update readme (#47)
    Co-authored-by: Johannes Pohl <johannes.pohl@badaix.de>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
+const host = process.env.REACT_APP_SNAPSERVER_HOST || window.location.host;
+
 let config = {
-    baseUrl: (window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host
-}
+  baseUrl: (window.location.protocol === "https:" ? "wss://" : "ws://") + host,
+};
 
 export { config };
