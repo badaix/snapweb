0948f3d13fb2344bdc9dd5ac8077ac7497f91076
badaix <johannes.pohl@badaix.de>
2025-03-01 20:41:57 +0000

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+  },
   base: './',
   plugins: [react(),
   VitePWA({
